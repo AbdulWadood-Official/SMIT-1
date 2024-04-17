38509ab6b8860ab755584ff6786179e2d536167b
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-04-17 14:40:20 +0500

    solution 4 completed

--- Python_Chai/05_funtions/04_solution.py
+++ Python_Chai/05_funtions/04_solution.py
@@ -1,1 +1,11 @@
 # Function returns multiple values:
+import math
+def circle_calculate(radius):
+    area = math.pi *radius**2
+    circumference = 2 * math.pi * radius
+    return area, circumference
+
+a,c = circle_calculate(3)
+
+print("Area:",round(a,2), "Circumference:",round(c,2))
+
