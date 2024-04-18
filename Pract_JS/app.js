cbc9e5a707a5f651c59f07159f125d1dc2278a53
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-04-18 16:23:43 +0500

    solving javascript question

---
+++ Pract_JS/app.js
@@ -0,0 +1,23 @@
+// Assignment:
+// **************Q1. *String Length Calculation**********************:
+//    Write a program to calculate the length of a given string.
+
+var fruit = ["Mango","Apple","Orange"];
+console.log(fruit.length);
+
+// Q2. *String to Uppercase*:
+//    Convert a string to uppercase.
+let text_uppercase = "javascript is case-sentitive"
+console.log(text_uppercase.toUpperCase());
+
+// Q3. *String to Lowercase*:
+//    Convert a string to lowercase.
+let text_lowercase = "Conversion To Lower";
+console.log(text_lowercase.toLowerCase());
+
+
+// 4. *Count Specific Character*:
+//    Count the occurrences of a specific character within a string.
+let Count_word = "I like Butterchillies";
+console.log(Count_word)
+
