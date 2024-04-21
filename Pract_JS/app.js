b30be7877619ed0a8e71f528a883c82d4a982cad
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-04-21 12:19:46 +0500

    problem solving

--- Pract_JS/app.js
+++ Pract_JS/app.js
@@ -18,19 +18,44 @@
 
 // 4. *Count Specific Character*:
 //    Count the occurrences of a specific character within a string.
-var string = prompt("Enter the string: ");
-var letter = prompt("Enter a letter:");
-let strlen = string.length;
+// var string = prompt("Enter the string: ");
+// var letter = prompt("Enter a letter:");
+// let strlen = string.length;
 
-var counter = 0;
-for (var i=0; i< strlen ; i++){
-    if(string[i] == letter){
-        counter++;
-    }
-}
-document.write(`${string} => ${letter} => ${counter}`);
+// var counter = 0;
+// for (var i=0; i< strlen ; i++){
+//     if(string[i] == letter){
+//         counter++;
+//     }
+// }
+// document.write(`${string} => ${letter} => ${counter}`);
 
 // Q5. *Substring Extraction*:
 //    Write a program that extracts a substring from a given string using a start and end index provided by the user.
+// var extract_str = "Javascript is versatile and popular language"
+// console.log(extract_str);
+// console.log(extract_str.substring(10));
+// console.log(extract_str.substring(0,11));
 
 
+// Q6. *Capitalize Each Word*:
+//     Write a program to capitalize the first letter of each word in a given string.
+// In this question we used 3 functions charAt(),toUpperCase(),slice()
+// var string = prompt("Enter a string");
+// console.log(string);
+// var first_word = string.charAt(0);
+// console.log(first_word);
+// console.log(first_word.toUpperCase());
+// var rem = string.slice(1);
+// console.log(rem);
+// var caps = first_word.toUpperCase() + rem;
+// console.log(`${caps}`);
+
+
+// Q7. *Trim Whitespace*:
+//     Remove leading and trailing whitespaces from a string.
+let remove_space = " Welcome to Javascript Programming "
+console.log(remove_space.trim());
+
+// 8. *Count Vowels and Consonants*:
+//     Count the number of vowels and consonants in a given string.
