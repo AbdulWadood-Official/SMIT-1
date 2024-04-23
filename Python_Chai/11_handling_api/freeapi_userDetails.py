c6d706a6cb59315056a4742541e62db0a1cc1893
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-04-23 22:47:06 +0500

    Handling api completed project

---
+++ Python_Chai/11_handling_api/freeapi_userDetails.py
@@ -0,0 +1,25 @@
+import requests
+
+def fetch_random_userapi():
+    url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["success"] and "data" in data:
+        user_data = data["data"]
+        username = user_data["login"]["username"]
+        usercountry = user_data["location"]["country"]
+        return username, usercountry
+    else:
+        raise Exception ("Failed to fetch user data")
+
+def main():
+    try:
+        username, country = fetch_random_userapi()
+        print(f"Username:{username} \n Country: {country}")
+    except Exception as e:
+        print(str(e))
+
+if __name__ == "__main__":
+    main()
+
