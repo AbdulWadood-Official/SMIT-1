49b70a2fa44d07f8311d697bdc6eda9cf2a9321a
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-04-26 23:12:22 +0500

    completed project

--- Python_Chai/11_handling_api/freeapi_userDetails.py
+++ Python_Chai/11_handling_api/freeapi_userDetails.py
@@ -12,7 +12,7 @@ def fetch_random_userapi():
         return username, usercountry
     else:
         raise Exception ("Failed to fetch user data")
-# Adding function in it and  
+# Adding function  
 def main():
     try:
         username, country = fetch_random_userapi()
