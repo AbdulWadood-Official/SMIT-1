6cc790b3a716bf16d63e6a4db8db5b3b3b800439
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-05-03 23:50:47 +0500

    completed asign

--- Pract_JS/app.js
+++ Pract_JS/app.js
@@ -217,7 +217,7 @@
 
 // // Call the function and store the result
 // let isUnique = hasUniqueCharacters(inputString);
-// Insha Allah, I will crack the 
+// Insha Allah, I will  
 // // Print the result
 // if (isUnique) {
 //     console.log("The string has all unique characters.");
