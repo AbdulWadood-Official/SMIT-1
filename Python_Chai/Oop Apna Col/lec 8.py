319b4ce18600bcb90c1f300147eaee58aa00a4a1
Abdul-Wadood-0317 <abdulwadood0317@gmail.com>
2024-05-12 23:53:19 +0500

    practo

--- Python_Chai/Oop Apna Col/lec 8.py
+++ Python_Chai/Oop Apna Col/lec 8.py
@@ -1,5 +1,15 @@
 class Student:
-    name = "Ahmed"
+    
+    def __init__ (self,name):
+        self.name = name
+        print("add new student in database:")
 
-s1 = Student()
+s1 = Student("Ali")
 print(s1.name)
+
+s2 = Student("Ahmed")
+print(s2.name)
+
+print( Student("Sara"))
+# print(s3.name)
+
